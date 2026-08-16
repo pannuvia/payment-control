63bd30313103ef074784415a7ca198138fc5ef51
Pannuvia Soares Monteiro <pannuvia@hotmail.com>
2026-08-16 19:27:26 -0300

    docs: adiciona comentarios e JSDoc no authHelper.js

--- test/helpers/authHelper.js
+++ test/helpers/authHelper.js
@@ -8,9 +8,13 @@
  *  - Reutilizar a autenticação em qualquer suíte de testes de forma centralizada.
  */
 
+// Importa o cliente HTTP Supertest para efetuar as requisições para a API
 const request = require('supertest');
+
+// Importa a URL base da API configurada de forma centralizada
 const { API_URL } = require('../config/env'); 
 
+// Definition da Mutation GraphQL parametrizada para a operação de login
 const MUTATION_LOGIN = `
   mutation Login($email: String!, $senha: String!) {
     login(email: $email, senha: $senha) {
@@ -18,7 +22,15 @@ const MUTATION_LOGIN = `
     }
   }`;
 
+/**
+ * Função assíncrona para autenticar na API e obter o token JWT.
+ * 
+ * @param {string} [email='admin@admin.com'] - E-mail do usuário (padrão: credencial admin)
+ * @param {string} [senha='123456'] - Senha do usuário (padrão: credencial admin)
+ * @returns {Promise<string>} Retorna a string do Token JWT gerado
+ */
 async function obterToken(email = 'admin@admin.com', senha = '123456') {
+    // Realiza a chamada HTTP POST para o endpoint /graphql enviando a query e as variáveis
     const response = await request(API_URL)
         .post('/graphql')
         .send({
@@ -26,11 +38,14 @@ async function obterToken(email = 'admin@admin.com', senha = '123456') {
             variables: { email, senha }
         });
 
+    // Verifica se o GraphQL retornou um array de erros (ex: credenciais inválidas ou erro de servidor)
     if (response.body.errors) {
         throw new Error(`Falha ao obter token de autenticação: ${JSON.stringify(response.body.errors)}`);
     }
 
+    // Extrai e retorna apenas a string do token JWT contida na resposta da API
     return response.body.data.login.token;
 }
 
+// Exporta a função obterToken para ser reutilizada nos testes
 module.exports = { obterToken };
